0fcfeb826e75c8f42bc2bcccb7239aaefdefb8de
ryan soh <179121852+ryansoh-42@users.noreply.github.com>
2025-04-03 13:55:06 +0800

    clean up

--- services/itinerary/itinerary_service.py
+++ services/itinerary/itinerary_service.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from icalendar import Calendar, Event
 import requests
 import json
 import os
